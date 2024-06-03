8f94c06ff1e73b9591d14895e5afc4e6251f75d9
Htoo Maung Thait <htoomaungthait@gmail.com>
2024-06-03 18:30:04 +0700

    46 Abstract classes overview

---
+++ type_script_crash_course/05-inheritance/05-abstract-classes/01-some-abstract-classes/src/Rectangle.ts
@@ -0,0 +1,40 @@
+import { Shape } from "./Shape";
+
+export class Rectangle extends Shape {
+
+    constructor(
+        private _theX: number,
+        private _theY: number,
+        private _theWidth: number,
+        private _theLength: number,
+    ) {
+        super(_theX, _theY);
+    }
+
+    public get width(): number {
+        return this._theWidth;
+    }
+    public set width(value: number) {
+        this._theWidth = value;
+    }
+
+
+    public get length(): number {
+        return this._theLength;
+    }
+
+    public set length(value: number) {
+        this._theLength = value;
+    }
+
+
+    public getInfo(): string {
+        return super.getInfo() + `, width = ${this.width}, length = ${this.length}`;
+    }
+
+    public calculateArea(): number {
+        return this.width * this.length;
+    }
+
+
+}
